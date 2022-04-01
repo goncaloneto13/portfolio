92e37ca24a1527102b8583d1b4c9ddfee5d5e5a4
goncaloneto13 <g.neto13hotmail.com>
2022-04-01 18:43:06 -0300

    link

--- src/componenents/Contato/contato.js
+++ src/componenents/Contato/contato.js
@@ -10,7 +10,7 @@ export default function Contato(){
 
                 <h2><AiOutlineMail ></AiOutlineMail> E-mail: <a href="mailto:g.neto137@gmail.com?subject=Olá%20Gonçalo,%20encontrei%20seu%20email%20pelo%20portifólio">g.neto137@gmail.com</a></h2>
                 <h2><AiOutlineWhatsApp color={'green'}></AiOutlineWhatsApp> Telefone: <a target="_blank" href=' https://api.whatsapp.com/send?phone=5586988547825&text=Olá%20Gonçalo,%20encontrei%20seu%20número%20pelo%20portifólio'>(86) 98854-7825</a>  </h2>
-                <h2><AiFillLinkedin/> Linkedin: <a href="https://www.linkedin.com/in/gonçalo-a-955b49112/">https://www.linkedin.com/in/gonçalo-a-955b49112/</a></h2>
+                <h2><AiFillLinkedin/> Linkedin: <a className ='linkedin' href="https://www.linkedin.com/in/gonçalo-a-955b49112/">https://www.linkedin.com/in/gonçalo-a-955b49112/</a></h2>
 
             </div>
 
